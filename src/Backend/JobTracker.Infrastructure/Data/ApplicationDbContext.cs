322cb399cd7a47d75dac4b05eaed8acba22c9f9e
Sáfár Erik <safar.erik13@gmail.com>
2026-01-15 16:36:53 +0100

    feat: implement ApplicationDbContext with DbSet properties for entities

---
+++ src/Backend/JobTracker.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+namespace JobTracker.Infrastructure.Data;
+
+using JobTracker.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public DbSet<Company> Companies {get; set;}
+
+    public DbSet<Skill> Skills {get; set;}
+
+    public DbSet<JobApplication> JobApplications {get; set;}
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+
+        // Here we set, that the JobbApplication table-s AppliedDate column will
+        // receive the latest timestamp in the database automatically 
+        modelBuilder.Entity<JobApplication>()
+        .Property(j => j.AppliedDate)
+        .HasDefaultValueSql("CURRENT_TIMESTAMP");
+    }
+
+
+}

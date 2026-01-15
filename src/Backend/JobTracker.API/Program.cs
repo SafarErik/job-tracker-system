52575df0709a10fdc0eb26ff3909efaf5827af01
Sáfár Erik <safar.erik13@gmail.com>
2026-01-15 16:41:08 +0100

    feat: configure PostgreSQL connection and register ApplicationDbContext

--- src/Backend/JobTracker.API/Program.cs
+++ src/Backend/JobTracker.API/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using JobTracker.Infrastructure.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Read the coonection string from the appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// We register The DBContext to the postgresql here
+builder.Services.AddDbContext<JobTracker.Infrastructure.Data.ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();

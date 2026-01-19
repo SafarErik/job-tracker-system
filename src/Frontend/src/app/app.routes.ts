96a0889346492d93dd32c1bea7204bc5963f46fb
Sáfár Erik <safar.erik13@gmail.com>
2026-01-19 17:07:16 +0100

    feat: define application routes for JobList and JobForm components

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { JobList } from './components/job-list/job-list';
+import { JobForm } from './components/job-form/job-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: JobList }, // Főoldal
+  { path: 'new', component: JobForm }, // Új hozzáadása oldal
+  { path: '**', redirectTo: '' }, // Minden más a főoldalra visz
+];

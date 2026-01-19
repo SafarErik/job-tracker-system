5affe952149bab094f244ceb6ca76e32e59e0eb9
Sáfár Erik <safar.erik13@gmail.com>
2026-01-19 18:13:39 +0100

    feat: update app layout with navigation header and footer, and fix JobForm import in routing

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { JobList } from './components/job-list/job-list';
-import { JobForm } from './components/job-form/job-form';
+import { JobFormComponent } from './components/job-form/job-form';
 
 export const routes: Routes = [
   { path: '', component: JobList }, // Főoldal
-  { path: 'new', component: JobForm }, // Új hozzáadása oldal
+  { path: 'new', component: JobFormComponent }, // Új hozzáadása oldal
   { path: '**', redirectTo: '' }, // Minden más a főoldalra visz
 ];

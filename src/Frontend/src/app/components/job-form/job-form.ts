d6fc0175b7b3896f368cb846deb858d20a23e62d
Sáfár Erik <safar.erik13@gmail.com>
2026-01-19 18:20:31 +0100

    fix: remove unused RouterLink import in JobFormComponent

--- src/Frontend/src/app/components/job-form/job-form.ts
+++ src/Frontend/src/app/components/job-form/job-form.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 // Services
 import { ApplicationService } from '../../services/application';

525ad1d6321df10bedebd950b34b704926b370a0
Sáfár Erik <safar.erik13@gmail.com>
2026-01-20 15:14:36 +0100

    feat: implement modern navigation styles and update routing for companies and details

--- src/Frontend/src/app/app.routes.ts
+++ src/Frontend/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { JobList } from './components/job-list/job-list';
 import { JobFormComponent } from './components/job-form/job-form';
+import { CompanyDetailsComponent } from './components/company-details/company-details';
+import { CompanyListComponent } from './components/company-list/company-list';
 
 export const routes: Routes = [
   { path: '', component: JobList }, // Home page - displays all applications
   { path: 'new', component: JobFormComponent }, // Create new application
   { path: 'edit/:id', component: JobFormComponent }, // Edit existing application
+  { path: 'companies', component: CompanyListComponent }, // Companies list page
+  { path: 'company/:id', component: CompanyDetailsComponent }, // Company details page
   { path: '**', redirectTo: '' }, // Redirect all unknown routes to home
 ];

d4ecfc696fcddc10bc2eec06996f4676a530bfb9
Sáfár Erik <safar.erik13@gmail.com>
2026-01-24 22:18:46 +0100

    feat: add environment configuration script for API and Google Client ID

---
+++ src/Frontend/public/env.js
@@ -0,0 +1,7 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // Default values (can be overridden at deploy time)
+  window.__env.API_BASE_URL = 'https://jobtracker-api.azurewebsites.net/api';
+  window.__env.GOOGLE_CLIENT_ID = '';
+})(this);

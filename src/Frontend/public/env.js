399112dde6a83496099ff5cf87fb46b26b5c0568
Sáfár Erik <safar.erik13@gmail.com>
2026-01-25 12:08:45 +0100

    feat: add client-side environment variable configuration in env.js

--- src/Frontend/public/env.js
+++ src/Frontend/public/env.js
@@ -2,6 +2,6 @@
   window.__env = window.__env || {};
 
   // Default values (can be overridden at deploy time)
-  window.__env.API_BASE_URL = 'http://localhost:5053/api';
-  window.__env.GOOGLE_CLIENT_ID = 'test-client-id';
+  window.__env.API_BASE_URL = 'https://jobtracker-api-b7dsamfycgfudvfm.polandcentral-01.azurewebsites.net/api';
+  window.__env.GOOGLE_CLIENT_ID = '';
 })(this);

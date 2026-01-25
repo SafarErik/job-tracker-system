1724d3e368040e9e1a32c519c867027994c6c87e
Sáfár Erik <safar.erik13@gmail.com>
2026-01-25 18:31:16 +0100

    chore: Delete unspecified file.

--- src/Frontend/public/env.js
+++
@@ -1,7 +0,0 @@
-(function (window) {
-  window.__env = window.__env || {};
-
-  // Default values (can be overridden at deploy time)
-  window.__env.API_BASE_URL = 'http://localhost:5053/api';
-  window.__env.GOOGLE_CLIENT_ID = '';
-})(this);

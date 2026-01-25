86fcbe6c25125e1bd9a047e7811eab2804730663
Sáfár Erik <safar.erik13@gmail.com>
2026-01-25 11:52:43 +0100

    feat: Add `env.js` to define frontend API base URL and Google client ID.

--- src/Frontend/public/env.js
+++ src/Frontend/public/env.js
@@ -2,6 +2,6 @@
   window.__env = window.__env || {};
 
   // Default values (can be overridden at deploy time)
-  window.__env.API_BASE_URL = '';
-  window.__env.GOOGLE_CLIENT_ID = '';
+  window.__env.API_BASE_URL = 'http://localhost:5053/api';
+  window.__env.GOOGLE_CLIENT_ID = 'test-client-id';
 })(this);
